5fafdf72c9101a640fa0502b2f604cc2ebf2463d
政客君 <kejun@zheng.biz>
2022-04-08 11:33:34 +0800

    git ignore and deploy script

---
+++ bin/release.ps1
@@ -0,0 +1,7 @@
+$cmdFile = $MyInvocation.MyCommand.Definition
+$cmdPath = [System.IO.Path]::GetDirectoryName($cmdFile)
+$logFile = $cmdPath+"\release" + (Get-Date -Format yyMMddHHmmss) + ".log"
+Set-Location  $cmdPath/..
+$mvnCmd = mvn -X clean source:jar deploy
+$mvnCmd | tee $logFile
+pause
